34097af58082a6991103477d2084631b50a9ff90
Rodrigo Martins <rooxalot@gmail.com>
2020-04-09 15:07:41 -0300

    Criado CRUD de reposítórios e incrementação de Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repo);
+
+  return response.status(201).json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  const updateRepo = {
+    title,
+    url,
+    techs,
+  }
+
+  Object.assign(repositories[repoIndex], updateRepo);
+
+  return response.json(repositories[repoIndex]);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    response.status(400).json({ error: 'Repository not found.' });
+  }
+  
+  repositories[repoIndex].likes++;
+
+  response.status(201).json(repositories[repoIndex]);
 });
 
 module.exports = app;
